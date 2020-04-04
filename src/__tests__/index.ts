cbd0042a6d6359f8ca841a957539b0dc3c6b1191
teitei-tk <teitei.tk@gmail.com>
2020-04-04 21:20:24 +0900

    feat(src/__tests__/index.ts): fix anime.annict.js package spec

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -3,6 +3,6 @@ const packageJSON = require("./../../package.json");
 
 describe("annict.anime.js", () => {
   it("package", () => {
-    expect(packageJSON.version).toBe("0.0.1");
+    expect(packageJSON.version).toBe("0.0.0-development");
   });
 });
